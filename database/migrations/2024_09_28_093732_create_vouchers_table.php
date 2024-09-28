502244f3573faa067a26d68c70a8a7da3b140189
khiemgiao <zazgia@gmail.com>
2024-09-28 17:50:59 +0700

    vouchers table

---
+++ database/migrations/2024_09_28_093732_create_vouchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('discount_type');
+            $table->tinyInteger('status');
+            $table->decimal('discount');
+            $table->string('qty');
+            $table->timestamps('start');
+            $table->timestamps('end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
