2d67500e7ebbc5a34cb4fafa4a9b7ca53500d541
TuanVu <vuntph37641@fpt.edu.vn>
2024-10-01 23:32:44 +0700

    abc

--- resources/views/admin/brands/index.blade.php
+++ resources/views/admin/brands/index.blade.php
@@ -2,46 +2,89 @@
 @extends('admin.layouts.master')
 
 @section('title')
-    Danh sách Brands
+    Brands
 @endsection
+{{-- @push('style')
+<style>
+    #bannerCarousel {
+        max-width: 400px; /* Adjust the width to your preference */
+        margin: 0 auto; /* Centers the carousel horizontally */
+    }
+    #bannerCarousel img {
+        max-height: 500px; /* Adjust the height to your preference */
+        object-fit: cover; /* Ensures the images fit within the container without distortion */
+    }
+</style>
+@endpush --}}
 {{-- section: định nghĩa nội dung của section --}}
 @section('content')
-    <div class="card">
-        <h4 class="card-header">Danh sách</h4>
-        <div class="card-body">
-            <a href="{{ route('admin.brands.create') }}" class="btn btn-success">Thêm </a>
-            <table class="table">
-                <thead>
-                    <th>STT</th>
-                    <th>Logo</th>
-                    <th>Name</th>
-                    <th>slug</th>
-                    <th>Hành động</th>
-                </thead>
-                <tbody>
-                    @foreach ($brands as $index => $item)
-                        <tr>
-                            <td>{{ $index + 1 }}</td>
-                            <td>
-                                <img src="{{ Storage::url($item->logo) }}" width="100" height="100" alt="">
-                            </td>
-                            <td>{{ $item->name }}</td>
-                            <td>{{ $item->slug }}</td>
-                            <td>
-                                <a href="{{ route('admin.brands.edit', $item->id) }}">
-                                    <button class="btn btn-warning">Sửa</button>
-                                </a>
-                                <form action="{{ route('admin.brands.destroy', $item->id) }}" method="post">
-                                    @method('DELETE')
-                                    @csrf
-                                    <button type="submit" onclick="return confirm('ban co muon xoa khong?')" class="btn btn-danger">Xóa</button>
-                                </form>
-                                
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
+<div class="row">
+    <div class="col-xl-12">
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">Danh sách @yield('title')</h4>
+                {{-- <a href="#" class="btn btn-danger mx-2">Xóa</a> --}}
+                <a href="{{ route('admin.brands.create') }}" class="btn btn-success">Thêm mới</a>
+            </div>
+            <!-- end card header -->
+
+            <div class="card-body">
+                {{-- <p class="text-muted mb-4">Use .<code>table-striped-columns</code> to add zebra-striping to any table column.</p> --}}
+
+                <div class="live-preview">
+                    <div class="table-responsive table-card">
+                        <table class="table align-middle table-nowrap table-striped-columns mb-0">
+                            <thead class="table-light">
+                                <tr>
+                                    <th scope="col" style="width: 46px;">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="cardtableCheck">
+                                            <label class="form-check-label" for="cardtableCheck"></label>
+                                        </div>
+                                    </th>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Hình ảnh</th>
+                                    <th scope="col">Tên thương hiệu</th>
+                                    <th scope="col">Đại diện thương hiệu</th>
+                                    <th scope="col" style="width: 150px;">Hành động</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($brands as $index => $item)
+                            <tr>
+                                <td>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value=""id="itemCheck{{ $item }}">
+                                        <label class="form-check-label" for="itemCheck{{ $item }}"></label>
+                                    </div>
+                                </td>
+                                <td>{{ $index + 1 }}</td>
+                                <td>
+                                    <img src="{{ Storage::url($item->logo) }}" width="100" height="100" alt="">
+                                </td>
+                                <td>{{ $item->name }}</td>
+                                <td>{{ $item->slug }}</td>
+                                <td>
+                                    <a href="{{ route('admin.brands.edit', $item->id) }}">
+                                        <button class="btn btn-sm btn-warning">Sửa</button>
+                                    </a>
+                                    <form action="{{ route('admin.brands.destroy', $item->id) }}" method="post" class="d-inline">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" onclick="return confirm('Bạn có muốn xóa không ???')" class="btn btn-sm btn-danger">Xóa</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div><!-- end col -->
+</div>
+    
+        
 @endsection
