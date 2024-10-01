f6bb2a5319fc568a61f1dd4757c1cdeb7d521238
hoanghai2904 <vuhoanghai20044@gmail.com>
2024-10-02 00:08:36 +0700

    update

---
+++ resources/views/admin/brands/add.blade.php
@@ -0,0 +1,34 @@
+{{-- extends: Chỉ định layout được sử dụng --}}
+@extends('admin.layouts.master')
+
+@section('title')
+    Thêm 
+@endsection
+
+{{-- section: định nghĩa nội dung của section --}}
+@section('content')
+    <div class="card">
+        <h4 class="card-header">Thêm </h4>
+        <div class="card-body">
+            <form action="{{ route('admin.brands.store') }}" method="POST" enctype="multipart/form-data">
+                {{-- 1 cơ chế bảo mật của laravel --}}
+                @csrf
+                <div class="mb-3">
+                    <label for="" class="form-label">Nhãn hiệu:</label>
+                    <input type="file" class="form-control" name="logo">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Tên thương hiệu:</label>
+                    <input type="text" class="form-control" name="name" placeholder="">
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">Slug:</label>
+                    <input type="text" class="form-control" name="slug" placeholder="">
+                </div>
+                <div class="mb-3 d-flex justify-content-center">
+                    <button type="submit" class="btn btn-success">Thêm mới</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,16 @@
 <?php
 
-use App\Http\Controllers\BannerController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CancelledOrderController;
+use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\OrderStatusController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BannerController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\VoucherController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +27,15 @@ Route::prefix('admin')->as('admin.')->group(function() {
     Route::get('/', function () {
         return view('admin.dashboard');
     })->name('dashboard');
-    Route::resource('categories', CategoryController::class);
-    Route::resource('products', ProductController::class);
+    Route::resource('orders',OrderController::class);
+    Route::resource('transactions', TransactionController::class);
+    Route::resource('order-items', OrderItemController::class);
+    Route::resource('order-statuses', OrderStatusController::class);
+    Route::resource('cancelled-orders', CancelledOrderController::class);
+
+    Route::resource('brands', BrandsController::class);
+
+    Route::resource('vouchers',VoucherController::class);
 
     Route::get('/list',function () {
         return view('admin.list.index');
