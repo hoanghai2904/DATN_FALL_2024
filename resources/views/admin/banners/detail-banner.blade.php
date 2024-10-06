98cec7a0f0c0e5cb1b0ac090c6772d209eb7843c
hoanghai2904 <vuhoanghai20044@gmail.com>
2024-10-06 15:34:58 +0700

    detail

--- resources/views/admin/banners/detail-banner.blade.php
+++ resources/views/admin/banners/detail-banner.blade.php
@@ -17,6 +17,8 @@
         <p>
             <strong>URL:</strong> 
             <a href="{{ $banner->url }}" target="_blank">Đường Dẫn Banner</a>
+            <a href="{{ $banner->url }}" target="_blank">Đường Dẫn Banner</a>
+            <a href="{{ $banner->url }}" target="_blank">Đường Dẫn Banner</a>
             {{-- <a href="{{ $banner->url }}" target="_blank">{{ $banner->url }}</a> --}}
         </p>
 
