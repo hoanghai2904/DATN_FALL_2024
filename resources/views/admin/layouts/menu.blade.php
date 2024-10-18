00a3a9089c63c0ab5e97ce91e59f2e5bfb7b0839
huybui268 <huybqph36479@fpt.edu.vn>
2024-10-18 10:23:43 +0700

    update

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -25,161 +25,162 @@
         </button>
     </div>
     <div id="scrollbar">
-        <div class="container-fluid">
-            <div id="two-column-menu">
-            </div>
-            <ul class="navbar-nav" id="navbar-nav">
-                <li class="menu-title"><span data-key="t-menu">Tổng quát</span></li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Bảng thống kê</span>
-                    </a>
-
-                    <div class="collapse menu-dropdown" id="sidebarDashboards">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link"> Thống kê theo A</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link"> Thống kê theo B</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý danh mục</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#product" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Sản phẩm</span>
-                    </a>
-
-                    <div class="collapse menu-dropdown" id="product">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{ route('admin.products.index') }}" class="nav-link" data-key="t-analytics">
-                                    Quản lý sản phẩm</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('admin.products.index') }}" class="nav-link" data-key="t-analytics">
-                                    Quản lý biến thể</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="{{ route('admin.banners.listBanner') }}" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý banner</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="{{ route('admin.orders.index') }}" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý đơn hàng</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="{{ route('admin.contacts.index') }}" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý liên hệ</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#sale" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý khuyến mại</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="sale">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">Nếu có thì làm dropdow</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#news" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Bài viết</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="news">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">Quản lý danh mục</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">Quản lý bài viết</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#user" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">User</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="user">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="{{route('admin.listRole')}}" class="nav-link">Quản lý phân quyền</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{route('admin.listCusstomer')}}" class="nav-link">Quản lý người dùng</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{route('admin.listUser')}}" class="nav-link">Quản lý nhân viên</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#contact" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý liên hệ</span>
-                    </a>
-
-                    <div class="collapse menu-dropdown" id="contact">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">ádfsdfsf</a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#coment" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý bình luận</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="coment">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">abcd</a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link menu-link" href="#review" data-bs-toggle="collapse" role="button"
-                        aria-expanded="false" aria-controls="sidebarDashboards">
-                        <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Quản lý đánh giá</span>
-                    </a>
-                    <div class="collapse menu-dropdown" id="review">
-                        <ul class="nav nav-sm flex-column">
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">abcd</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
+    <div class="container-fluid">
+        <div id="two-column-menu">
         </div>
-        <!-- Sidebar -->
+        <ul class="navbar-nav" id="navbar-nav">
+            <li class="menu-title"><span data-key="t-menu">Tổng quát</span></li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sidebarDashboards" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-dashboard-2-line"></i> <span data-key="t-dashboards">Bảng thống kê</span>
+                </a>
+
+                <div class="collapse menu-dropdown" id="sidebarDashboards">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"> Thống kê theo A</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"> Thống kê theo B</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#" aria-controls="sidebarDashboards">
+                    <i class="ri-folder-3-line"></i> <span data-key="t-dashboards">Quản lý danh mục</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#product" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-shopping-bag-line"></i> <span data-key="t-dashboards">Sản phẩm</span>
+                </a>
+
+                <div class="collapse menu-dropdown" id="product">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="{{ route('admin.products.index') }}" class="nav-link" data-key="t-analytics">
+                                Quản lý sản phẩm</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('admin.products.index') }}" class="nav-link" data-key="t-analytics">
+                                Quản lý biến thể</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="{{ route('admin.banners.listBanner') }}" aria-controls="sidebarDashboards">
+                    <i class="ri-image-line"></i> <span data-key="t-dashboards">Quản lý banner</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="{{ route('admin.orders.index') }}" aria-controls="sidebarDashboards">
+                    <i class="ri-file-list-line"></i> <span data-key="t-dashboards">Quản lý đơn hàng</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="{{ route('admin.contacts.index') }}" aria-controls="sidebarDashboards">
+                    <i class="ri-phone-line"></i> <span data-key="t-dashboards">Quản lý liên hệ</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#sale" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-price-tag-3-line"></i> <span data-key="t-dashboards">Quản lý khuyến mại</span>
+                </a>
+                <div class="collapse menu-dropdown" id="sale">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Nếu có thì làm dropdow</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#news" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-article-line"></i> <span data-key="t-dashboards">Bài viết</span>
+                </a>
+                <div class="collapse menu-dropdown" id="news">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Quản lý danh mục</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Quản lý bài viết</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#user" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-user-line"></i> <span data-key="t-dashboards">User</span>
+                </a>
+                <div class="collapse menu-dropdown" id="user">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="{{route('admin.listRole')}}" class="nav-link">Quản lý phân quyền</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('admin.listCusstomer')}}" class="nav-link">Quản lý người dùng</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('admin.listUser')}}" class="nav-link">Quản lý nhân viên</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#contact" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-phone-line"></i> <span data-key="t-dashboards">Quản lý liên hệ</span>
+                </a>
+
+                <div class="collapse menu-dropdown" id="contact">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">ádfsdfsf</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#coment" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-chat-3-line"></i> <span data-key="t-dashboards">Quản lý bình luận</span>
+                </a>
+                <div class="collapse menu-dropdown" id="coment">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">abcd</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link menu-link" href="#review" data-bs-toggle="collapse" role="button"
+                    aria-expanded="false" aria-controls="sidebarDashboards">
+                    <i class="ri-star-line"></i> <span data-key="t-dashboards">Quản lý đánh giá</span>
+                </a>
+                <div class="collapse menu-dropdown" id="review">
+                    <ul class="nav nav-sm flex-column">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">abcd</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
     </div>
+    <!-- Sidebar -->
+</div>
+
     <div class="sidebar-background"></div>
 </div>
