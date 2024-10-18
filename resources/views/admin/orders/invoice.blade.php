c1332fb86951c94674d2ead54c41ee71f1969ea3
huybui268 <huybqph36479@fpt.edu.vn>
2024-10-19 01:08:04 +0700

    update

--- resources/views/admin/orders/invoice.blade.php
+++ resources/views/admin/orders/invoice.blade.php
@@ -31,7 +31,6 @@
     <div class="col-xxl-9">
         <div class="card" id="demo">
             <div class="row">
-     
                 <div class="col-lg-12">
                     <div class="card-body p-4">
                         <div class="row g-3">
@@ -49,7 +48,7 @@
                             </div>
                             <div class="col-lg-3 col-6">
                                 <p class="text-muted mb-2 text-uppercase fw-semibold">Tổng số tiền</p>
-                                <h5 class="fs-14 mb-0">$<span id="total-amount">{{ number_format($order->total_price, 2) }}</span></h5>
+                                <h5 class="fs-14 mb-0">${{ number_format($order->total_price, 0, ',', '.') }}₫</h5>
                             </div>
                         </div>
                     </div>
@@ -61,14 +60,14 @@
                                 <h6 class="text-muted text-uppercase fw-semibold mb-3">Địa chỉ thanh toán</h6>
                                 <p class="fw-medium mb-2" id="billing-name">{{ $order->name }}</p>
                                 <p class="text-muted mb-1" id="billing-address-line-1">{{ $order->user_address }}</p>
-                                <p class="text-muted mb-1"><span>Số điện thoại: +</span><span id="billing-phone-no">{{ $order->	user_phone }}</span></p>
+                                <p class="text-muted mb-1"><span>Số điện thoại: +</span><span id="billing-phone-no">{{ $order->user_phone }}</span></p>
                                 <p class="text-muted mb-0"><span>Chi tiết: </span><span id="billing-tax-no">{{ $order->user_note }}</span></p>
                             </div>
                             <div class="col-6">
                                 <h6 class="text-muted text-uppercase fw-semibold mb-3">Địa chỉ giao hàng</h6>
                                 <p class="fw-medium mb-2" id="billing-name">{{ $order->name }}</p>
                                 <p class="text-muted mb-1" id="billing-address-line-1">{{ $order->user_address }}</p>
-                                <p class="text-muted mb-1"><span>Số điện thoại: +</span><span id="billing-phone-no">{{ $order->	user_phone }}</span></p>
+                                <p class="text-muted mb-1"><span>Số điện thoại: +</span><span id="billing-phone-no">{{ $order->user_phone }}</span></p>
                                 <p class="text-muted mb-0"><span>Chi tiết: </span><span id="billing-tax-no">{{ $order->user_note }}</span></p>
                             </div>
                         </div>
@@ -77,53 +76,70 @@
                 <div class="col-lg-12">
                     <div class="card-body p-4">
                         <div class="table-responsive">
-                            <table class="table table-borderless text-center table-nowrap align-middle mb-0">
-                                <thead>
-                                    <tr class="table-active">
-                                        <th scope="col" style="width: 50px;">#</th>
-                                        <th scope="col">Chi tiết sản phẩm</th>
-                                        <th scope="col">Giá</th>
-                                        <th scope="col">Giá giảm</th>
-                                        <th scope="col">Quantity</th>
-                                        <th scope="col" class="text-end">Amount</th>
-                                    </tr>
-                                </thead>
-                                <tbody id="order-items-list">
-                                    @foreach($orderItems as $index => $item)
-                                        <tr>
-                                            <th scope="row">{{ $index + 1 }}</th>
-                                            <td>{{ $item->product_name}}</td>
-                                            <td>${{ number_format($item->product_price, 2) }}</td>
-                                            <td>${{ number_format($item->product_price_sale, 2) }}</td>
-                                            <td>{{ $item->qty }}</td>
-                                            <td class="text-end">${{ number_format($item->product_price * $item->qty, 2) }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                                <tfoot>
+                        <table class="table table-borderless text-center table-nowrap align-middle mb-0">
+                            <thead>
+                                <tr class="table-active">
+                                    <th scope="col" style="width: 50px;">#</th>
+                                    <th scope="col">Chi tiết sản phẩm</th>
+                                    <th scope="col">Giá</th>
+                                    <th scope="col">Số lượng</th>
+                                    <th scope="col" class="text-end">Tổng</th>
+                                </tr>
+                            </thead>
+                            <tbody id="order-items-list">
+                                @foreach($orderItems as $index => $item)
                                     <tr>
-                                        <th colspan="4" class="text-end">Total:</th>
-                                        <td class="text-end">${{ number_format($order->total_price, 2) }}</td>
+                                        <th scope="row">{{ $index + 1 }}</th>
+                                        <td>{{ $item->product_name }}</td>
+                                        <td>
+                                            @if($item->product_price_sale)
+                                                <span class="text-decoration-line-through">{{ number_format($item->product_price, 0, ',', '.') }}₫</span>
+                                                <span class="text-danger">{{ number_format($item->product_price_sale, 0, ',', '.') }}₫</span>
+                                            @else
+                                                {{ number_format($item->product_price, 0, ',', '.') }}₫
+                                            @endif
+                                        </td>
+                                        <td>{{ $item->qty }}</td>
+                                        <td class="text-end">
+                                            @if($item->product_price_sale)
+                                                {{ number_format($item->product_price_sale * $item->qty, 0, ',', '.') }}₫
+                                            @else
+                                                {{ number_format($item->product_price * $item->qty, 0, ',', '.') }}₫
+                                            @endif
+                                        </td>
                                     </tr>
-                                    <tr>
-                                        <th colspan="4" class="text-end">Tax:</th>
-                                        <td class="text-end">${{ number_format($order->tax, 2) }}</td>
-                                    </tr>
-                                    <tr>
-                                        <th colspan="4" class="text-end">Grand Total:</th>
-                                        <td class="text-end fw-medium">${{ number_format($order->grand_total, 2) }}</td>
-                                    </tr>
-                                </tfoot>
-                            </table>
+                                @endforeach
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th colspan="4" class="text-end">Tổng:</th>
+                                    <td class="text-end">{{ number_format($orderItems->sum(function($item) {
+                                        return $item->product_price_sale ? $item->product_price_sale * $item->qty : $item->product_price * $item->qty;
+                                    }), 0, ',', '.') }}₫</td>
+                                </tr>
+                                <tr>
+                                    <th colspan="4" class="text-end">Giảm giá:</th>
+                                    <td class="text-end">{{ number_format($order->discount_price ?? 0, 0, ',', '.') }}₫</td>
+                                </tr>
+                                <tr>
+                                    <th colspan="4" class="text-end">Phí vận chuyển:</th>
+                                    <td class="text-end">{{ number_format($order->shipping_fee, 0, ',', '.') }}₫</td>
+                                </tr>
+                                <tr>
+                                    <th colspan="4" class="text-end">Tổng cộng:</th>
+                                    <td class="text-end fw-medium">{{ number_format(($orderItems->sum(function($item) {
+                                        return $item->product_price_sale ? $item->product_price_sale * $item->qty : $item->product_price * $item->qty;
+                                    }) - ($order->discount_price ?? 0) + $order->shipping_fee), 0, ',', '.') }}₫</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-12">
                     <div class="card-body p-4 text-end border-top border-top-dashed">
-
                         <a href="javascript:window.print()" class="btn btn-success"><i class="ri-printer-line align-bottom me-1"></i> In</a>
-               
-                      
                     </div>
                 </div>            
             </div>
