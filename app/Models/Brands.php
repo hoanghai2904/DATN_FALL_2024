b4542c936e3052cd6939869fc764cee4cead1232
TuanVu <vuntph37641@fpt.edu.vn>
2024-11-17 01:28:38 +0700

    update

--- app/Models/Brands.php
+++ app/Models/Brands.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brands extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+
+    protected $filable = [
+        'logo',
+        'name',
+        'slug',
+];
+    public $table = 'brands';
+    public $timestamp = false;
+    public function createBrands($data) 
+    {
+        DB::table('brands')->insert($data);
+    }
+    public function updateBrands($data, $id) 
+    {
+        DB::table('brands')
+        ->where('id', $id)
+        ->update($data);  
+    }
 }
