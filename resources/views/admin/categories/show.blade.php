0b516a5d0fd3a41afa3bc2d1941ad65fb1233fab
TuanVu <vuntph37641@fpt.edu.vn>
2024-11-21 00:15:35 +0700

    update

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,14 +1,16 @@
-{{-- @extends('admin.layouts.master')
+@extends('admin.layouts.master')
 
 @section('title', 'Chi tiết danh mục')
 
 @section('content')
     <ul>
-        <li><strong>ID:</strong> {{$category->id}}</li>
-        <li><strong>Tên:</strong> {{$category->name}}</li>
-        <li><strong>Slug:</strong> {{$category->slug}}</li>
-        <li><strong>Trạng thái:</strong> 
-            {!! $category->status ? '<span class="badge bg-success">Hoạt động</span>' : '<span class="badge bg-danger">Không hoạt động</span>' !!}
+        <li><strong>ID:</strong> {{ $category->id }}</li>
+        <li><strong>Tên:</strong> {{ $category->name }}</li>
+        <li><strong>Slug:</strong> {{ $category->slug }}</li>
+        <li><strong>Trạng thái:</strong>
+            {!! $category->status
+                ? '<span class="badge bg-success">Hoạt động</span>'
+                : '<span class="badge bg-danger">Không hoạt động</span>' !!}
         </li>
     </ul>
-@endsection --}}
+@endsection
