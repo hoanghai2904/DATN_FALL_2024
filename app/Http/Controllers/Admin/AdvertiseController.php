1fb325e983afd76f2aced38d0b623bef2b03d6f7
khiemgiao <zazgia@gmail.com>
2024-11-25 20:56:43 +0700

    Advertise edit

--- app/Http/Controllers/Admin/AdvertiseController.php
+++ app/Http/Controllers/Admin/AdvertiseController.php
@@ -93,4 +93,62 @@ class AdvertiseController extends Controller
   
       return response()->json($data, 200);
     }
+  
+    public function edit($id)
+    {
+      $advertise = Advertise::where('id', $id)->first();
+      if(!$advertise) abort(404);
+      return view('admin.advertise.edit')->with('advertise', $advertise);
+    }
+  
+    public function update(Request $request, $id)
+    {
+      $validator = Validator::make($request->all(), [
+        'title' => 'required',
+        'at_home_page' => 'required',
+        'date' => 'required',
+      ], [
+        'title.required' => 'Tiêu đề quảng cáo không được để trống!',
+        'at_home_page.required' => 'Vị trí trang hiển thị phải được chọn!',
+        'date.required' => 'Ngày bắt đầu và kết thúc phải được chọn!',
+      ]);
+  
+      if ($validator->fails()) {
+        return back()
+          ->withErrors($validator)
+          ->withInput();
+      }
+  
+      //Xử lý ngày bắt đầu, ngày kết thúc
+      list($start_date, $end_date) = explode(' - ', $request->date);
+  
+      $start_date = str_replace('/', '-', $start_date);
+      $start_date = date('Y-m-d', strtotime($start_date));
+  
+      $end_date = str_replace('/', '-', $end_date);
+      $end_date = date('Y-m-d', strtotime($end_date));
+  
+      $advertise = Advertise::where('id', $id)->first();
+      $advertise->title = $request->title;
+  
+      if($request->hasFile('image')){
+        $image = $request->file('image');
+        $image_name = time().'_'.$image->getClientOriginalName();
+        $image->storeAs('images/advertises',$image_name,'public');
+        Storage::disk('public')->delete('images/advertises/' . $advertise->image);
+        $advertise->image = $image_name;
+      }
+  
+      $advertise->at_home_page = $request->at_home_page;
+      $advertise->start_date = $start_date;
+      $advertise->end_date = $end_date;
+  
+      $advertise->save();
+  
+      return redirect()->route('admin.advertise.index')->with(['alert' => [
+        'type' => 'success',
+        'title' => 'Thành Công',
+        'content' => 'Quảng cáo của bạn đã được cập nhật thành công.'
+      ]]);
+    }
   }
