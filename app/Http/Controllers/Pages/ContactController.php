1b0e587072301985ff94fe798472d1f8108f8b50
khiemgiao <zazgia@gmail.com>
2024-12-09 14:45:48 +0700

    contact

--- app/Http/Controllers/Pages/ContactController.php
+++ app/Http/Controllers/Pages/ContactController.php
@@ -8,26 +8,5 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function sendContact(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|email|max:255',
-            'message' => 'required|string',
-        ]);
-
-        $data = [
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'message' => $request->input('message'),
-        ];
-
-        sendContactMail::dispatch($data);
-
-        return redirect()->route('home_page')->with(['alert' => [
-            'type' => 'success',
-            'title' => 'Thành công',
-            'content' => 'Gửi liên hệ thành công'
-        ]]);
-    }
+   
 }
