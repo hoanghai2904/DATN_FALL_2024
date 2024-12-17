8d8958348810e96c451b38f8a8c97db2a3f9b5f4
TuanVu <vuntph37641@fpt.edu.vn>
2024-12-17 16:32:53 +0700

    a

--- resources/views/layouts/wishlist.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="support-wishlist mini-wishlist">
-    <a class="btn-support-wishlist" href="{{ route('show_wishlist') }}">
-        <i class="fa fa-heart" color: #FFFFFF;"></i>
-        <span class="cnt crl-bg count_item_pr">{{ session('wishlist', collect())->count() }}</span>
-    </a>
-</div>
-<div id="menu-overlay"></div>
