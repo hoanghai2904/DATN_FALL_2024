c60db5ab4465c3409739f06100632d295bd9ab59
phamHuy <hailabeo1@gmail.com>
2024-12-19 14:13:26 +0700

    fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -74,13 +74,13 @@ class LoginController extends Controller
             return redirect()->route('admin.dashboard')->with(['alert' => [
                 'type' => 'success',
                 'title' => 'Đăng nhập thành công',
-                'content' => 'Chào mừng bạn đến với trang quản trị website Shop Paws'
+                'content' => 'Chào mừng bạn đến với trang quản trị website Shop Pet'
             ]]);
         } else {
             return redirect()->route('home_page')->with(['alert' => [
                 'type' => 'success',
                 'title' => 'Đăng nhập thành công',
-                'content' => 'Chào mừng bạn đến với website Shop Paws của chúng tôi'
+                'content' => 'Chào mừng bạn đến với website Shop Pet của chúng tôi'
             ]]);
         }
     }
